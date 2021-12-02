13b601c25903ee1efefabf890ab0a2cf76b8f4df
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2021-12-03 01:17:05 +0530

    feat(day1): add part2 solution

--- puzzles/day01/index.js
+++ puzzles/day01/index.js
@@ -6,13 +6,29 @@ async function solutionDay01() {
   const inputData = await readInputByLine(inputsFilePath, (v) =>
     parseInt(v, 10),
   );
-  let result = 0;
-  for (let i = 1; i < inputData.length; i += 1) {
-    if (inputData[i] > inputData[i - 1]) {
-      result += 1;
+  const doesIncrement = (arrData) => {
+    let result = 0;
+    for (let i = 1; i < arrData.length; i += 1) {
+      if (arrData[i] > arrData[i - 1]) {
+        result += 1;
+      }
     }
+    return result;
+  };
+
+  const resultPart1 = doesIncrement(inputData);
+  console.log('solutionDay01::part1', resultPart1);
+
+  // Part 2
+  const threeSums = [];
+  for (let i = 0; i < inputData.length - 2; i += 1) {
+    threeSums.push(inputData[i] + inputData[i + 1] + inputData[i + 2]);
   }
-  console.log('solutionDay01::result', result);
+
+  const resultPart2 = doesIncrement(threeSums);
+  console.log('solutionDay01::part2', resultPart2);
+
+  return [resultPart1, resultPart2];
 }
 
 module.exports = {
